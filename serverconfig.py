b758173641ee93370127f0d38ac05a0dc3bb3021
Samuel Braikeh <samuel.braikeh@yahoo.fr>
2024-05-22 12:38:06 +0200

    update server

--- serverconfig.py
+++ serverconfig.py
@@ -5,8 +5,10 @@ import secrets
 from werkzeug.utils import secure_filename
 import os
 
+CONFIG_FILE = 'config.yml'
+
 UPLOAD_FOLDER = '/home/pi/uploads/'
-ALLOWED_EXTENSIONS = {'mp3', 'wav', 'wave'}
+ALLOWED_EXTENSIONS = {'mp3', 'wav', 'wave', 'tmp'}
 MAX_CONTENT_LENGTH = 16 * 1000 * 1000
 
 app = Flask(__name__)
@@ -28,27 +30,21 @@ def unmap_volume(mapped_volume):
     return volume
 
 def get_balise_dict_infos():
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    volume = int(config['INFOS']['default_volume'])
-    volume = unmap_volume(volume)
-    nom = config['INFOS']['nom']
-    lieu = config['INFOS']['lieu']
-    default_message = int(config['INFOS']['default_message'])
-    print(default_message)
-#    return {'balId':3434 ,'nom':nom, 'lieu':lieu, 'defaultMessage':default_message, 'volume':volume}
-    with open('config.yaml') as c:
-        config = yaml.load(c, Loader=yaml.SafeLoader) 
+    with open(CONFIG_FILE) as c:
+        config = yaml.load(c, Loader=yaml.SafeLoader)
     volume = int(config['INFOS']['volume'])
     volume = unmap_volume(volume)
     nom = config['INFOS']['nom']
     lieu = config['INFOS']['lieu']
     default_message = int(config['INFOS']['default_message'])
-    return {'balId':3434 ,'nom':nom, 'lieu':lieu, 'defaultMessage':default_message, 'volume':volume}
-
+    plages_horaires = config['INFOS']['plages_horaires']
+    return {'balId':3434 ,'nom':nom, 'lieu':lieu, \
+        'defaultMessage':default_message, \
+        'volume':volume, \
+        'plages_horaires':plages_horaires}
 
 def get_annonces_dict():
-    with open('config.yaml') as c:
+    with open(CONFIG_FILE) as c:
         config = yaml.load(c, Loader=yaml.SafeLoader)
     return config["ANNONCES"]
 
@@ -72,20 +68,17 @@ def query_infos():
 @app.route('/volume', methods=['POST'])
 def set_volume():
     try:
-   #     try:
         data = request.get_json(force=True)
-        print("data", data)
         volume = int(data.get("volume"))
-#        except:
- #           data2 = request.get_json(force=True)
-  #          volume = int(data2)
         mapped_volume = map_volume(volume)
-        print("mapped = ",mapped_volume)
-        config = configparser.ConfigParser()
-        config.read('config.ini')
-        config.set('INFOS', 'DEFAULT_VOLUME', str(mapped_volume))
-        with open('config.ini', 'w') as configfile:
-            config.write(configfile)
+        print("mapped volume = ", mapped_volume)
+        # save to configfile
+        with open(CONFIG_FILE) as c:
+            config = yaml.load(c, Loader=yaml.SafeLoader)
+        config['INFOS']['volume'] = mapped_volume
+        yaml_str = yaml.dump(config, sort_keys=False, Dumper=yaml.SafeDumper)
+        with open(CONFIG_FILE, "w") as c:
+            yaml.dump(config, c, sort_keys=False, Dumper=yaml.SafeDumper)
         return jsonify(get_balise_dict_infos()), 200
     except:
         return jsonify({'error':'failed to update volume'}), 400
@@ -96,15 +89,20 @@ def set_infos():
         data = request.get_json(force=True)
         nom = data.get("nom")
         lieu = data.get("lieu")
-        config = configparser.ConfigParser()
-        config.read('config.ini')
-        config.set('INFOS', 'NOM', nom)
-        config.set('INFOS', 'LIEU', lieu)
-        with open('config.ini', 'w') as configfile:
-            config.write(configfile)
+        # save to configfile
+        with open(CONFIG_FILE) as c:
+            config = yaml.load(c, Loader=yaml.SafeLoader)
+        config['INFOS']['nom'] = nom
+        config['INFOS']['lieu'] = lieu
+        with open(CONFIG_FILE, "w") as c:
+            yaml.dump(config, c, sort_keys=False, Dumper=yaml.SafeDumper)
         return jsonify(get_balise_dict_infos()), 200
     except:
         return jsonify({'error':'failed to update name or/and place'}), 400
+
+@app.route('/plages_horaires', methods=['POST'])
+def set_plages_horaires():
+    pass
 
 @app.route('/defaultmessage', methods=['POST'])
 def set_default_message():
@@ -112,11 +110,12 @@ def set_default_message():
         data = request.get_json(force=True)
         print(data)
         default_message = int(data.get("id_message"))
-        config = configparser.ConfigParser()
-        config.read('config.ini')
-        config.set('INFOS', 'DEFAULT_MESSAGE', str(default_message))
-        with open('config.ini', 'w') as configfile:
-            config.write(configfile)
+        # save to configfile
+        with open(CONFIG_FILE) as c:
+            config = yaml.load(c, Loader=yaml.SafeLoader)
+        config['INFOS']['default_message'] = default_message
+        with open(CONFIG_FILE, "w") as c:
+            yaml.dump(config, c, sort_keys=False, Dumper=yaml.SafeDumper)
         return jsonify(get_balise_dict_infos()), 200
     except:
         return jsonify({'error':'failed to update default message'}), 400
@@ -133,45 +132,126 @@ def add_annonce():
         id_annonce = int(data.get("id_annonce"))
         nom = data.get("nom")
         type = data.get("type")
-        filename = secrets.token_urlsafe(6) + "_" + str(id_annonce)
+        filename = data.get("filename")
         contenu = data.get("contenu")
         lang = data.get("lang")
         duree = data.get("duree")
-        with open('config.yaml') as c:
+        with open(CONFIG_FILE) as c:
             config = yaml.load(c, Loader=yaml.SafeLoader)
         new_annonce = {"id_annonce":id_annonce, "nom":nom, "type":type, \
-            #"filename":filename, \
+            "filename":filename,
             "contenu":contenu, "lang":lang, "duree":duree}
-        #config["ANNONCES"].append(new_annonce)
-
-        response_body = {"id_annonce":id_annonce, "nom":nom, "type":type, \
-            "contenu":contenu, "lang":lang, "duree":duree}
-        response_body.update({"upload_sound_url":"/annonce/upload_sound/" + filename})
-        return jsonify(response_body), 200
+        config["ANNONCES"].append(new_annonce)
+        with open(CONFIG_FILE, "w") as c:
+            yaml.dump(config, c, sort_keys=False, Dumper=yaml.SafeDumper)
+        response_body = new_annonce
+ #       token = secrets.token_urlsafe(6) + "_" + str(id_annonce)
+ #       response_body.update({"upload_sound_url":"/annonce/upload_sound/" + token})
+        return jsonify(response_body), 201
     except:
         return 'oups', 400
-#@app.route('/')
 
-@app.route('/annonce/upload_sound/<string:token>', methods=['POST'])
+@app.route('/annonce', methods=['POST'])
+def set_annonce():
+    try:
+        data = request.get_json(force=True)
+        print(data)
+        id_annonce = int(data.get("id_annonce"))
+        nom = data.get("nom")
+        type = data.get("type")
+        filename = data.get("filename")
+        contenu = data.get("contenu")
+        lang = data.get("lang")
+        duree = data.get("duree")
+        with open(CONFIG_FILE) as c:
+            config = yaml.load(c, Loader=yaml.SafeLoader)
+        annonces = config['ANNONCES']
+        # get annonce from id_annonce
+        index_annonce = next((i for i, item in enumerate(annonces) if item["id_annonce"] == id_annonce), None)
+        if index_annonce is not None:
+            updated_annonce = {"id_annonce":id_annonce, "nom":nom, "type":type, \
+                "filename":filename,
+                "contenu":contenu, "lang":lang, "duree":duree}
+            # save to configfile
+            config["ANNONCES"][index_annonce] = updated_annonce
+            with open(CONFIG_FILE, "w") as c:
+                yaml.dump(config, c, sort_keys=False, Dumper=yaml.SafeDumper)
+            return jsonify(updated_annonce), 200
+        else:
+            return jsonify({'error':'annonce not found'}), 404
+    except:
+        return jsonify({'error':'failed to send the sound test'}), 400
+
+@app.route('/annonce', methods=['DELETE'])
+def delete_annonce():
+    try:
+        data = request.get_json(force=True)
+        print(data)
+        id_annonce = int(data.get("id_annonce"))
+        with open(CONFIG_FILE) as c:
+            config = yaml.load(c, Loader=yaml.SafeLoader)
+        annonces = config['ANNONCES']
+        # get annonce from id_annonce
+        index_annonce = next((i for i, item in enumerate(annonces) if item["id_annonce"] == id_annonce), None)
+        if index_annonce is not None:
+            del annonces[id_annonces]
+            # save to configfile
+            with open(CONFIG_FILE, "w") as c:
+                yaml.dump(config, c, sort_keys=False, Dumper=yaml.SafeDumper)
+            return jsonify({'id_annonce':id_annonce}), 200
+        else:
+            return jsonify({'error':'annonce not found'}), 404
+    except:
+        return jsonify({'error':'failed to send the sound test'}), 400
+
+
+#@app.route('/annonce/upload_sound/<string:token>', methods=['POST'])
 @app.route('/annonce/upload_sound', methods=['POST'])
 def upload_sound():
     if True:
-        try: 
+        try:
             file = request.files['audiofile']
-            print("youpi")
             if file.filename == '':
                 return jsonify({'error': 'failed to upload audio file'}), 400
             print(file.filename)
             if file and allowed_file(file.filename):
-                print("ça avance hein")
                 filename = secure_filename(file.filename)
                 print(filename)
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'audio', filename))
-            return jsonify({"code":33}), 200 
+            return jsonify({"code":33}), 200
         except:
             return jsonify({'error':'failed to upload audio file'}), 400
     return jsonify({'error':'failed to upload audio file'}), 403
 
+@app.route('/test_sound', methods=['GET'])
+def test_balise():
+    try:
+        with open(CONFIG_FILE) as c:
+            config = yaml.load(c, Loader=yaml.SafeLoader)
+        default_message = int(config['INFOS']['default_message'])
+        annonces = config['ANNONCES']
+        # get annonce from default_message id
+        result = next((item for item in annonces if item["id_annonce"] == default_message), None)
+        # check if filename exists
+        if "filename" in result:
+            filename = result["filename"]
+#            print(filename)
+        else:
+            filename = 'Balise1-0.wav'
+        print(filename)
+        audio_file_path = os.path.join(app.config['UPLOAD_FOLDER'], 'audio', filename)
+        volume = int(config['INFOS']['volume'])
+        volume = int(volume*1024/100)
+        # play sound
+        if (result["type"]=="AUDIO") :
+            vlc_command = f"vlc --play-and-exit {audio_file_path}"
+            os.system(vlc_command)
+        else :
+            aplay_command = f"aplay {audio_file_path}"
+            os.system(aplay_command)
+        return jsonify({'success':'sound test sent'}), 200
+    except:
+        return jsonify({'error':'failed to send the sound test'}), 400
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
